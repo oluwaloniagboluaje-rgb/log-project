43e8674a0f9ce6e8857cd9119925f94fa7d8e326
Nimi <oluwaloniagboluaje@gmail.com>
2026-03-12 03:07:59 -1000

    refactor: update CORS preflight request handling to match path pattern

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('/{*path}', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
